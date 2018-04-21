9d2efa5e15516710c1c47ec85402fbeab54ee960
jiaxiaochen <jiaxiaochen@duiba.com.cn>
2018-04-21 16:42:13 +0800

    图片

--- src/views/knowledge/pages/order.vue
+++ src/views/knowledge/pages/order.vue
@@ -63,12 +63,10 @@
             <el-table-column prop="itemId" label="商品ID" width="80"></el-table-column>
             <el-table-column label="商品信息" width="300">
               <template slot-scope="scope">
-                <div class="img-container">
-                  <img :src="scope.row.itemImage" alt="" class="goods-list-img">
-                  <div class="goods-mask">{{scope.row.type}}</div>
-                </div>
-                <span v-if="scope.row.itemName.length > 15" v-text="scope.row.itemName" class="two-ellipsis"></span>
-                <span v-else v-text="scope.row.itemName" class="goods-word"></span>
+                <div v-if="scope.row.itemImage" class="img-box" v-bind:style="{backgroundImage:'url('+scope.row.itemImage+')',backgroundSize: 'contain',backgroundPosition: 'center'}"></div>
+                <div  v-else class="img-box" v-bind:style="{backgroundImage:'url(//yun.dui88.com/yoofans/images/201804/noClassImg.png)',backgroundSize: 'contain',backgroundPosition: 'center'}"></div>
+                 <span v-if="scope.row.itemName.length > 12" v-text="scope.row.itemName" class="twoLines ln37 w150"></span>
+                 <span v-else v-text="scope.row.itemName" class="goods-word"></span>
               </template>
             </el-table-column>
             <el-table-column label="单价(元)" >
@@ -347,19 +345,9 @@ export default {
       position:absolute;
     }
   }
-  .two-ellipsis{
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    width: 200px;
-    float: left;
-    margin-left: 10px;
-  }
   .goods-word{
     margin-left: 10px;
-    line-height: 50px;
+    line-height: 75px;
   }
   .btn-disabled {
     color: #fff;
@@ -372,6 +360,32 @@ export default {
     vertical-align: bottom;
     animation: ellipsis 2s infinite;
     content: "\2026"; 
+  }
+  .twoLines{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    float: left;
+    margin-left: 10px;
+  }
+  .ln75{
+    line-height:75px;
+  }
+  .ln37{
+    line-height:37px;
+  }
+  .w150{
+    width:150px;
+  }
+  .img-box{
+    overflow: hidden;
+    width: 75px;
+    height: 75px;
+    display: inline-block;
+    float: left;
+    background-repeat: no-repeat;
   }
   @keyframes ellipsis {
       from {

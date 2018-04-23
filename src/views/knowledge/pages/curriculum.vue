b7eef1e3c3b1cee5151cbeb0c96789c2406e2724
dujishi <kein.du@cnoneteam.com>
2018-04-23 20:51:16 +0800

    文本去除视频添加

--- src/views/knowledge/pages/curriculum.vue
+++ src/views/knowledge/pages/curriculum.vue
@@ -424,6 +424,28 @@
         editor.customConfig.uploadFileName = 'imageFile'
         editor.customConfig.withCredentials = false;
         editor.customConfig.debug=true;
+        editor.customConfig.menus = [
+          'head',  // 标题
+          'bold',  // 粗体
+          'fontSize',  // 字号
+          'fontName',  // 字体
+          'italic',  // 斜体
+          'underline',  // 下划线
+          'strikeThrough',  // 删除线
+          'foreColor',  // 文字颜色
+          'backColor',  // 背景颜色
+          'link',  // 插入链接
+          'list',  // 列表
+          'justify',  // 对齐方式
+          'quote',  // 引用
+          'emoticon',  // 表情
+          'image',  // 插入图片
+          'table',  // 表格
+//          'video',  // 插入视频
+          'code',  // 插入代码
+          'undo',  // 撤销
+          'redo'  // 重复
+        ];
         editor.customConfig.uploadImgHooks = {
           customInsert: function (insertImg, result, editor) {
             // 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）

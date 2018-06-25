65c0ed1494c46c075f3768e6cbcee1a4d802363f
jiaxiaochen <jiaxiaochen@duiba.com>
2018-06-25 18:19:55 +0800

    id显示

--- src/views/knowledge/pages/addActivity.vue
+++ src/views/knowledge/pages/addActivity.vue
@@ -20,7 +20,7 @@
 
             <el-form-item label="奖励优惠券" prop="coupon">
 							<el-select v-model="activityForm.coupon">
-								<el-option v-for="item in masterOptions" :key="item.couponTemplateId" :label="item.name" :value="item.couponTemplateId">
+								<el-option v-for="item in masterOptions" :key="item.couponTemplateId" :label="item.name + '/' + item.id" :value="item.couponTemplateId">
 								</el-option>
 							</el-select>
             </el-form-item>
